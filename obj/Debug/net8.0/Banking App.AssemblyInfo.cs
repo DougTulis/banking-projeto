3e8190cdc41b013e4206b7438567064a4bc08c72
Douglas <dengratulis@gmail.com>
2024-11-24 16:39:06 -0300

    Incluido eventos no projeto

--- obj/Debug/net8.0/Banking App.AssemblyInfo.cs
+++ obj/Debug/net8.0/Banking App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d91bce5d508388638130712df04999846c500a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0bbd7214ac6cd86001447298dc03f2fd93564d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
