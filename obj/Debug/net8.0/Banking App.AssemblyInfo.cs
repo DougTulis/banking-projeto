9a66bb3f3ef55f4882ebb1d2db9be360b83d516e
Douglas <dengratulis@gmail.com>
2024-11-24 17:20:46 -0300

    Separado/Isolado algumas classes confome letra S do solid

--- obj/Debug/net8.0/Banking App.AssemblyInfo.cs
+++ obj/Debug/net8.0/Banking App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0bbd7214ac6cd86001447298dc03f2fd93564d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8190cdc41b013e4206b7438567064a4bc08c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
